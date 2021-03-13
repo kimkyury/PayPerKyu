a5fe1904cc4fdc8279cc0caf94a9d39c6760a4c3
kimkyury <mandarining0918@gmail.com>
2021-03-13 23:03:35 +0900

    [Calculation]Addition of Array

--- AddArray.java
+++ AddArray.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class AddArray {
 
     static int n;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,13 +22,18 @@ public class AddArray {
         int sum = add(intArr);
 
         System.out.print("\n-----계산결과------\n");
-        System.out.print("배열의 원소들의 합: " + sum);
+        System.out.println("배열의 원소들의 합: " + sum);
+
+        // T(n)=n
+        System.out.print("계산을 위한 덧셈 실행 횟수: " + cnt);
         System.out.print("\n------------------");
     }
 
     static int add(int[] intArr) {
+        cnt = 0;
         int result = 0;
         for (int i = 0; i < n; i++) {
+            cnt++;
             result += intArr[i];
         }
         return result;
