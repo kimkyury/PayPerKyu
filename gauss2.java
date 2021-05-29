18a553050d1f9bff3b8cd598e8aa786025247edc
kimkyury <mandarining0918@gmail.com>
2021-05-29 17:40:27 +0900

    Create [gauss elimination]

--- gauss2.java
+++ gauss2.java
@@ -1,13 +1,14 @@
-
 public class gauss2 {
-    private static double [][] equation; //방정식을 담는 공간
+    public static int n;            // 방정식 개수(동시에 미지수의 개수) 
+    private static double [][] eq; //방정식을 담는 공간
     private static double [] b;
-    public static int n; // 방정식 개수(동시에 미지수의 개수) 
-    public static double [] answer; //내가 출력할 해
+    
     public static double [] x; //각 열의 x값
     public static double [] s; //각 행의 최대 계수값
+    public static double [] answer; 
+    
+    public static int er;           // 특이시스템 판정
     public static double tol = 0.000001;  //특이시스템 검출을 위한 최소한계값
-    public static int er;
 
     // 주어진 방정식 값을 2차원 배열에 대입시킨다
     public static void setting(double [][] info, double [] right){
@@ -15,65 +16,93 @@ public class gauss2 {
         n = info.length; //선형방정식의 개수
 
         /* 2차원, 1차원 배열을 생성하고 초기화시키기 */
-        equation = new double[n][n]; //해당 크기로 방정식[좌변] 공간할당
-        b= new double [n]; //선형방정식의 개수만큼 해[우변] 공간할당
-        x = new double [n]; //미지수의 개수만큼 할당
-        
-        answer = new double[n]; // 선형방정식의 개수만큼 정답arr 공간할당
+        eq = new double[n][n]; //해당 크기로 방정식[좌변] 공간할당
+        b= new double [n];           //선형방정식의 개수만큼 해[우변] 공간할당
+        x = new double [n];          //미지수의 개수만큼 할당
+        answer = new double[n];      // 선형방정식의 개수만큼 정답arr 공간할당
         for(int i =0; i<n; i++){
             for (int j =0; j<n; j++){
-                equation[i][j] = info[i][j];
+                eq[i][j] = info[i][j];
             }
-            b[i] = right[i]; // 선형방정식의 해
+            b[i] = right[i];    //선형방정식의 해
         }
 
-        er =0;           // 특이시스템 판단을 위한 er
-        s = new double [n];     // 축적화를 위한 s[]설정
+        System.out.print("\n========입력한 연립방정식=========\n");
+        display();
+
+
+        er = 0;                 // 특이시스템 판단을 위한 er
+        s = new double [n];     // 방정식 개수 공간 할당
 
         /* 각 선형방정식(i)의 가장 큰 계수값(S[i]) 찾기 */
-        for ( int i =1; i<n; i++){
-            s[i] = Math.abs(equation[i][1]); 
-            for( int j =2; j<n; j++){
-                if ( Math.abs(equation[i][j]) > s[i]){
-                    s[i] = Math.abs(equation[i][j]);
+        for ( int i =0; i<n; i++){
+            s[i] = Math.abs(eq[i][0]); 
+            for( int j = 1; j<n; j++){
+                if ( Math.abs(eq[i][j]) > s[i]){
+                    s[i] = Math.abs(eq[i][j]);  //최대 계수값을 가지도록 교체
                 }
             }
         }
-        Eliminate(); // 전진소거 시작
-        if( er != -1){ //특이시스템이 아님을 확인하고
-            Substitute(); //후진대입 시작
+
+        Eliminate();        // 전진소거 시작
+        if( er != -1){      // 특이시스템이 아님을 확인하고
+            Substitute();   // 후진대입 시작
+        }
+    }
+
+
+    public static void Eliminate(){
+        for(int k = 0; k < n-1; k++){
+            //가장 큰 계수가 pivot방정식이 될 수 있도록 각 행을 pivoting시킴
+            pivoting(k);
+
+            /* 특이시스템 검출 */
+            if( Math.abs(eq[k][k] / s[k]) < tol){
+                er = -1;
+            }
+
+            for(int i = k+1; i < n; i++){
+                double factor = eq[i][k] /eq[k][k];
+                for(int j= k; j < n; j++){
+                    eq[i][j] = eq[i][j] - factor * eq[k][j];
+                }
+                b[i] = b[i] - factor * b[k];
+            }
+            System.out.print("\n========피봇팅 & x[" + k + "]전진소거 결과=========\n");
+            display();
+        }
+
+        /* 특이시스템 검출 */
+        if( Math.abs(eq[n-1][n-1]/s[n-1]) < tol){
+            er = -1;
         }
     }
 
     /* 해당 2차원 배열에 대한  pivoting (행의 위치 바꾸기) */
-    // 매개변수는 현재 진행중인,  [pivot방정식]인 행의 번호임.
-    public static void pivoting(int k){
+    public static void pivoting(int k){  // k = [피봇방정식]의 행 번호, 0 ~ n-1까지
         int p = k;
-        double big = equation[k][k];
+        double big = Math.abs(eq[k][k]/s[k]); // 피봇계수의 상대적인 값
         
-        /* 축적화 진행 */
+        /* 축적화 진행, k행과 i행의 비교 */
         //[pivot방정식] 아래의 식들 중, pivot계수보다 큰 게 있는지 확인
-        for( int i= k+1; i <n; i++){
-            double dummy = Math.abs(equation[i][k]/s[i]);
-            //더 크다면 pivot값 교체
+        for( int i= k+1; i < n; i++){
+            double dummy = Math.abs(eq[i][k]/s[i]); 
+            // 더 크다면 pivot값 교체
             if(dummy > big){
                 big = dummy;
                 p = i;
             }
         }
 
-        /* pivot값이 실제로 교체될 시, 그 기준으로 재정렬 */
+        /* 바뀌었다면 행 교체 */
         if( p != k){
-
-            //p행과 k행의 각 열(j) 계수 교환
-            for(int j = k; j<n; j++){
-                double dummy = equation[p][j];
-                equation[p][j]= equation[k][j];
-                equation[k][j] = dummy;
+            for(int j = k; j < n; j++){   // p행과 k행의 미지수들 교체
+                double dummy = eq[p][j];
+                eq[p][j]= eq[k][j];
+                eq[k][j] = dummy;
             }
 
-            //p행과 k행 방정식의 해 교환
-            double dummy = b[p]; 
+            double dummy = b[p];        // p행과 k행의 방정식 해 교체
             b[p] = b[k];
             b[k] = dummy;
 
@@ -83,61 +112,47 @@ public class gauss2 {
             s[k] = dummy;
         }
     }
-
-    public static void Eliminate(){
-        for(int k = 1; k < n-1; k++){
-            //가장 큰 계수가 pivot방정식이 될 수 있도록 각 행을 pivoting시킴
-            pivoting(k);
-
-            /* 특이시스템 검출하기 */
-            if( Math.abs(equation[k][k] / s[k]) < tol){
-                er = 0;
-            }
-
-            for(int i = k+1; i < n; i++){
-                double factor = equation[i][k] /equation[k][k];
-                for(int j= k+1; j<n; j++){
-                    equation[i][j] = equation[i][j] - factor * equation[k][j];
-                }
-                b[i] = b[i] -factor * b[k];
-            }
-        }
-
-        /* 특이시스템 검출 */
-        // 피봇계수가 상대적으로 너무 작은 값이면 특이시스템으로 간주
-        if( Math.abs(equation[n][n]/s[n])<tol){
-            er = -1;
-        }
-    }
     
     public static void Substitute(){
-        // 마지막, x[n]을 구하기
-        x[n] = b[n] / equation[n][n];
+        // 마지막 미지수 구하기
+        x[n-1] = b[n-1] / eq[n-1][n-1];
 
         // 끝의 방정식으로부터 상향식으로 x[i] (i= n... 1)을 구하기
-        for(int i=n-1; i>1; i--){
+        for(int i = n-2; i >= 0; i--){
             double sum =0;
-            for(int j=i+1; j<n; j++){
-                sum += equation[i][j]*x[j];
+            for(int j = i+1; j < n; j++){
+                sum += eq[i][j]*x[j];
             }
-            x[i] = (b[i]-sum)/equation[i][i];
+            x[i] = (b[i]-sum)/eq[i][i];
+        }
+    }
+
+    public static void display(){
+        for(int i =0; i<n; i++){
+            for(int j =0; j<n; j++){
+                System.out.print(eq[i][j] + " x[" + j + "] ");
+                if( j<n-1)
+                    System.out.print("+ ");
+                else
+                    System.out.print("= ");
+            }
+            System.out.print(b[i] +"\n");
         }
     }
 
     public static void printing(){
-        System.out.print("----------결과---------\n");
+        System.out.print("\n========최종 값=========\n");
 
         for(int i=0 ;i<n; i++){
-            System.out.print("x" + 9 + ": " + x[i] + "\n" );
-        
+            System.out.print("x" + i+ ": " + x[i] + "\n" );
         }
     }
 
-    public static void main(){
-        
+    public static void main(String [] args){
         double [][] info = new double[][] {{1,2,-1},{5,2,2},{-3, 5, -1}};
-        double [] right = {2,9,1};
+        double [] right = new double [] {2,9,1};
         setting(info, right);
+        
         printing();
     }
 }
